feed8272473fbfa9c323fc624594d6bef52ed061
maert <clnmaert@yandex.ru>
2023-02-23 00:24:05 +0300

    doing exercises

--- exercises/06_control_structures/task_6_2.py
+++ exercises/06_control_structures/task_6_2.py
@@ -12,3 +12,19 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+userip = input('Ввидите ваш айпи: \n')
+userip_list = userip.split('.')
+
+if 1 < int(userip_list[0]) < 223:
+    print('unicast')
+elif 1 < int(userip_list[0]) < 239:
+    print('multicast')
+elif userip == '255.255.255.255':
+    print('local broadcast')
+elif userip == '0.0.0.0':
+    print('unassigned')
+else:
+    print('unused')
+print('=' * 30)
+print('Ай да я! Ай да молодец')
+print('!' * 30)
