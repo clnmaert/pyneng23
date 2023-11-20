e08cb2342480a516c8f54fa534085d858cf89f8b
clnadmin <clnmaert@yandex.ru>
2023-11-20 18:26:34 +0300

    c

--- exercises/12_useful_modules/task_12_2.py
+++ exercises/12_useful_modules/task_12_2.py
@@ -34,3 +34,31 @@
  '172.21.41.129', '172.21.41.130', '172.21.41.131', '172.21.41.132']
 
 """
+import subprocess
+
+def convert_ranges_to_ip_list(bunch_of_ips):
+    result = []
+    for i in bunch_of_ips:
+        if '-' in i:
+            if i.count('.') == 6:
+                firstip = i.split('-')[0]
+                lastip = i.split('-')[-1]
+                base_ip = firstip.split('.')
+                first_three_octets = f'{base_ip[0]}.{base_ip[1]}.{base_ip[2]}.'
+                #print(f'firstip {firstip}\nlastip {lastip}\nfirst_three_octets {first_three_octets}')
+                #print(f'first {firstip.split[-1]}\nlast {lastip.split[-1]}')
+                for r in range(int(firstip.split('.')[-1]), int(lastip.split('.')[-1])+1):
+                    result.append(f'{first_three_octets}{r}')
+            else:
+                base_ip = i.split('.')
+                last_octet_range = base_ip[-1]
+                first_three_octets = f'{base_ip[0]}.{base_ip[1]}.{base_ip[2]}.'
+                for s in range(int(last_octet_range[0]), int(last_octet_range[-1])+1):
+                    result.append(f'{first_three_octets}{s}')
+        else:
+            result.append(i)
+    return result
+if __name__ == "__main__":
+    print(convert_ranges_to_ip_list(['8.8.4.4', '1.1.1.1-3', '172.21.41.128-172.21.41.132']))
+
+#ai da ya!
